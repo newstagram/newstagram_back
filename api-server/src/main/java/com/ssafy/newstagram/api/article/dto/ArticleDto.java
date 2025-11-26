e9394be3bc5c828f4e7035cac99a03da9d79a3b7
hyunjo01 <hyunjo10217@gmail.com>
2025-11-26 17:47:58 +0900

    [feat] - ArticleDto 생성

--- api-server/src/main/java/com/ssafy/newstagram/api/article/dto/ArticleDto.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/article/dto/ArticleDto.java
@@ -1,5 +1,32 @@
 package com.ssafy.newstagram.api.article.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.sql.Timestamp;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleDto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private String description;
+    private String url;
+    private String thumbnailUrl;
+    private String author;
+    private Timestamp createAt;
+    private Timestamp updateAt;
+    private String category;
+    
 }
