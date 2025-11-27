c4e73bde8bf846f8225c51b07e02987e05797fc3
김현목 <dabinchi388@naver.com>
2025-11-27 18:12:45 +0900

    [feat] - user repository에 회원가입 관련 메서드 추가

--- api-server/src/main/java/com/ssafy/newstagram/api/users/repository/UserRepository.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/users/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.ssafy.newstagram.api.users.repository;
 
-public interface UserRepository {
+import com.ssafy.newstagram.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
 }
