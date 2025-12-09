0c4fba3a5f440c99c3bcfb3b1575e9e68cb0d251
hyunjo01 <hyunjo10217@gmail.com>
2025-12-09 20:41:13 +0900

    [feat] - ClusteringService: 클러스터별 인덱스 그룹핑

--- rss-collector/src/main/java/com/ssafy/newstagram/rss/clustering/service/ClusteringService.java
+++ rss-collector/src/main/java/com/ssafy/newstagram/rss/clustering/service/ClusteringService.java
@@ -6,7 +6,10 @@ import com.ssafy.newstagram.rss.vo.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +34,28 @@ public class ClusteringService {
         int minPts = 3;
         int[] labels = dbscanService.clustering(embeddings, eps, minPts);
 
+        // 4) 클러스터별 index 그룹핑
+        Map<Integer, List<Integer>> clusterGroups = groupByCluster(labels);
+
+    }
+
+
+    /**
+     * 라벨 배열을 기반으로 클러스터별 인덱스 그룹핑
+     */
+    private Map<Integer, List<Integer>> groupByCluster(int[] labels) {
+        Map<Integer, List<Integer>> clusterMap = new HashMap<>();
+
+        for (int i = 0; i < labels.length; i++) {
+            int label = labels[i];
+
+            // noise는 스킵
+            if (label == dbscanService.NOISE_INDEX) continue;
+
+            clusterMap.computeIfAbsent(label, k -> new ArrayList<>()).add(i);
+        }
+
+        return clusterMap;
     }
 
 }
