5423ecc9b5354a00e607615fe539f7215f1724cd
psmoon7304 <psmoon7304@naver.com>
2025-12-22 13:59:39 +0900

    [feat] - 사용자 초기 관심사 Kafka DLT Listener 추가

--- logging-server/src/main/java/com/ssafy/newstagram/logging/feature/deadletter/DeadLetterEventListener.java
+++ logging-server/src/main/java/com/ssafy/newstagram/logging/feature/deadletter/DeadLetterEventListener.java
@@ -7,7 +7,16 @@ import org.springframework.kafka.annotation.KafkaListener;
 @Slf4j
 public class DeadLetterEventListener {
     @KafkaListener(topics = KafkaTopic.Log.INTERACTION_DLT, groupId = "dlt-test-group")
-    public void listenDLT(String message) {
-        log.error("[Kafka] DLT - 모든 재시도 실패. Dead Letter 수신: {}", message);
+    public void listenArticleClickDLT(String message) {
+        logErrorMsg(KafkaTopic.Log.INTERACTION_DLT, message);
+    }
+
+    @KafkaListener(topics = KafkaTopic.Log.SURVEY_SUBMIT_DLT, groupId = "dlt-test-group")
+    public void listenSurveySubmitDLT(String message) {
+        logErrorMsg(KafkaTopic.Log.SURVEY_SUBMIT_DLT, message);
+    }
+
+    public void logErrorMsg(String topic, String msg) {
+        log.error("[Kafka] {} - 모든 재시도 실패. Dead Letter 수신: {}",KafkaTopic.Log.INTERACTION_DLT, msg);
     }
 }
