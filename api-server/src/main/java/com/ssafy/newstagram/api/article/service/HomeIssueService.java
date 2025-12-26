fc824e15d5d42b5a3440e17e295e588d5024d64e
hyunjo01 <hyunjo10217@gmail.com>
2025-12-26 10:54:49 +0900

    [feat] - HomeIssueService: 로그 추가

--- api-server/src/main/java/com/ssafy/newstagram/api/article/service/HomeIssueService.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/article/service/HomeIssueService.java
@@ -21,6 +21,7 @@ public class HomeIssueService {
     public List<ArticleDto> findByEmbeddingSimilarity(String embedding, int limit, LocalDateTime startDate) {
         List<Article> articles = articleRepository.findByEmbeddingSimilarity(embedding, limit, startDate);
         log.info("[HomeIssueService] startDate:{}", startDate);
+        log.info("[HomeIssueService] articles.size:{}", articles.size());
         // Article 리스트를 ArticleDto 리스트로 변환
         return articles.stream()
                 .map(this::toArticleDto)  // toArticleDto 메소드 사용
